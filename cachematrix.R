0b93337c194e02a6d10ea6e195e988cf4ca1fff9
Lyubomyr Luchechko <luchechko@gmail.com>
2014-09-15 21:15:19 -0500

    Adding functions
    Added all functions and some comments. Tested functions successfully.
    Still need to add more comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Coursera programming assignment 2.
+#This script contains to functions which allow to set a matrix, calculate inverse matrix, 
+#store inverse matrix in cache and modify initial matrix with subsequent recalculation of inverse matrix.
 
-## Write a short comment describing this function
-
+#The first function - makeCacheMatrix allows to create 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseM <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseM <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inverseM <<- solve
+  getinverse <- function() inverseM
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  inverseM <- x$getinverse()
+  if(!is.null(inverseM)) {
+    message("getting cached data")
+    return(inverseM)                     ## Return a matrix that is the inverse of 'x'
+  }
+  data <- x$get()
+  inverseM <- solve(data, ...)
+  x$setinverse(inverseM)
+  inverseM
 }
